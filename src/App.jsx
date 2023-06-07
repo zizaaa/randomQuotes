19855d57af7b650b1eedbe6b01f65760bf6bd6ff
zizaaa <winfourteen@gmail.com>
2023-06-07 16:59:49 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState,useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [quotes, setQuotes] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+      const quotes_url = "https://dummyjson.com/quotes";
+
+      useEffect(()=>{
+          const fetchData = async()=>{
+            try{
+              const fetchData = await fetch(quotes_url);
+              const jsonData = await fetchData.json();
+              const data = await jsonData;
+              
+              setData(data);
+            
+            }catch(err){
+              console.error(err)
+            }
+        }
+        fetchData();
+      },[])
+
+      const generateQuotes=()=>{
+        setIsLoading(true)
+
+        setTimeout(()=>{
+          const random = Math.floor(Math.random() * data.quotes.length)
+          const randomQuotes = data.quotes[random]
+          setQuotes(randomQuotes);
+          setIsLoading(false)
+        },2000)
+      }
+      
+  return (
+        <div className="card-container">
+            <div className="quotes">
+              <h1>{`" ${quotes.quote} "`}</h1>
+              <h4>{`-${quotes.author}`}</h4>
+            </div>
+            <div className="buttons">
+                <button className="generateBtn" onClick={generateQuotes}><span><i className="fa-solid fa-rotate-right" id={isLoading ? "rotate" : ""}></i></span> Generate</button>
+                <button className="copyBtn"><span><i className="fa-solid fa-copy"></i></span> Copy</button>
+            </div>
+        </div>
+  )
+}
+
+export default App
